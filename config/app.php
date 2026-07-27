f2f83e7cbc622484ac002c764435972a2930820c
Dikall <111861924+Dikall@users.noreply.github.com>
2026-07-28 06:01:46 +0700

    Update APP_URL to production environment

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'monev-production-584c.up.railway.app'),
 
     'asset_url' => env('ASSET_URL', '/'),
 
